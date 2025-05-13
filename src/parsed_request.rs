6490aec26c403b5ea3ae696097f80c97b25726ff
rmb122 <me@rmb122.com>
2025-05-14 01:17:14 +0800

    feat: request body parse done

--- src/parsed_request.rs
+++ src/parsed_request.rs
@@ -1,25 +1,32 @@
 use std::{collections::HashMap, net::SocketAddr};
 
 use axum::{body::Body, extract::Query, http::Request};
+use encoding_rs::Encoding;
 use futures::TryStreamExt;
+use multer::Multipart;
+use multimap::MultiMap;
 use serde_json::Value;
 use tokio::io::AsyncReadExt;
-use tokio_util::io::StreamReader;
+use tokio_util::io::{ReaderStream, StreamReader};
 
+#[derive(Debug)]
 pub enum ParsedRequestBody {
     None,
     Raw(Vec<u8>),
-    Form(HashMap<String, Vec<String>>),
+    Form(
+        MultiMap<String, String>,
+        MultiMap<String, (String, Vec<u8>)>,
+    ), // (普通的 post form, file)
     Json(Value),
-    // File(HashMap<String, Multipart>),
 }
 
+#[derive(Debug)]
 pub struct ParsedRequest {
     pub client_addr: SocketAddr,
     pub method: String,
     pub path: String,
-    pub headers: HashMap<String, Vec<String>>,
-    pub params: HashMap<String, Vec<String>>,
+    pub headers: MultiMap<String, String>,
+    pub params: MultiMap<String, String>,
 
     pub parsed_body: ParsedRequestBody,
 }
@@ -33,23 +40,18 @@ impl ParsedRequest {
             headers: request
                 .headers()
                 .iter()
-                .fold(HashMap::new(), |mut headers, kv| {
+                .fold(MultiMap::new(), |mut headers, kv| {
                     let key = Self::unify_header_key(kv.0.to_string());
                     let value = String::from_utf8_lossy(kv.1.as_bytes()).to_string();
-                    if let Some(entry) = headers.get_mut(&key) {
-                        entry.push(value);
-                    } else {
-                        headers.insert(key, vec![value]);
-                    };
-
+                    headers.insert(key, value);
                     headers
                 }),
             params: if let Ok(query) =
-                Query::<HashMap<String, Vec<String>>>::try_from_uri(request.uri())
+                Query::<MultiMap<String, String>>::try_from_uri(request.uri())
             {
                 query.0
             } else {
-                HashMap::new()
+                MultiMap::new()
             },
             parsed_body: ParsedRequestBody::None,
         };
@@ -64,7 +66,7 @@ impl ParsedRequest {
         .read_to_end(&mut body)
         .await?;
 
-        parsed_request.parse_body(body);
+        parsed_request.parse_body(body).await;
 
         Ok(parsed_request)
     }
@@ -88,26 +90,50 @@ impl ParsedRequest {
     }
 
     fn parse_content_type(content_type: &str) -> (String, HashMap<String, String>) {
-        let contnet_type = content_type.to_ascii_lowercase();
-
         let parsed_content_type;
         let mut attrs = HashMap::new();
 
-        let parts: Vec<&str> = contnet_type.split(";").collect();
-        parsed_content_type = parts[0].to_owned();
+        let parts: Vec<&str> = content_type.split(";").map(|x| x.trim()).collect();
+        parsed_content_type = parts[0].to_owned().to_lowercase();
 
         for part in parts.iter().skip(1) {
-            if let Some((left, right)) = part.split_once("=") {
-                attrs.insert((*left).to_owned(), (*right).to_owned());
+            if part.len() == 0 {
+                continue;
+            }
+
+            if let Some((left, mut right)) = part.split_once("=") {
+                if right.len() >= 2
+                    && ((right.starts_with("'") && right.ends_with("'"))
+                        || (right.starts_with('"') && right.ends_with('"')))
+                {
+                    right = &right[1..right.len() - 1];
+                }
+
+                attrs.insert((*left).to_owned().to_ascii_lowercase(), (*right).to_owned());
             } else {
-                attrs.insert((*part).to_owned(), String::new());
+                attrs.insert((*part).to_owned().to_ascii_lowercase(), String::new());
             }
         }
 
         (parsed_content_type, attrs)
     }
 
-    fn parse_body(&mut self, body: Vec<u8>) {
+    fn try_decode<T: AsRef<str>>(content: &[u8], charset: Option<T>) -> String {
+        let charset = match charset {
+            None => return String::from_utf8_lossy(content).to_string(),
+            Some(charset) => charset,
+        };
+
+        if let Some(encoding) =
+            Encoding::for_label(charset.as_ref().to_ascii_lowercase().as_bytes())
+        {
+            return encoding.decode_with_bom_removal(content).0.to_string();
+        } else {
+            return String::from_utf8_lossy(content).to_string();
+        }
+    }
+
+    async fn parse_body(&mut self, body: Vec<u8>) {
         let content_type = match self.headers.get("Content-Type") {
             Some(content_type) => content_type,
             None => {
@@ -117,7 +143,77 @@ impl ParsedRequest {
             }
         };
 
-        let (content_type, attrs) = Self::parse_content_type(&content_type[0]);
-        if content_type.ends_with("json") {}
+        let (content_type, attrs) = Self::parse_content_type(content_type);
+
+        // try json
+        if content_type.ends_with("/json") || content_type.ends_with("+json") {
+            let body = Self::try_decode(&body, attrs.get("charset"));
+
+            if let Ok(value) = serde_json::from_str::<Value>(&body) {
+                self.parsed_body = ParsedRequestBody::Json(value);
+                return;
+            }
+        }
+
+        // try x-www-form-urlencoded
+        if content_type.ends_with("/x-www-form-urlencoded") {
+            let body = Self::try_decode(&body, attrs.get("charset"));
+
+            let mut form = MultiMap::<String, String>::new();
+            for (key, value) in form_urlencoded::parse(body.as_bytes()) {
+                form.insert(key.to_string(), value.to_string());
+            }
+            self.parsed_body = ParsedRequestBody::Form(form, MultiMap::new());
+            return;
+        }
+
+        // try form-data
+        if let (true, Some(boundary)) =
+            (content_type == "multipart/form-data", attrs.get("boundary"))
+        {
+            println!("{:?}", boundary);
+            let mut multipart = Multipart::new(ReaderStream::new(body.as_slice()), boundary);
+            let mut form = MultiMap::<String, String>::new();
+            let mut file = MultiMap::<String, (String, Vec<u8>)>::new();
+
+            while let Ok(Some(mut field)) = multipart.next_field().await {
+                let name = if let Some(name) = field.name() {
+                    name.to_owned()
+                } else {
+                    continue;
+                };
+
+                let filename = field.file_name().map(|x| x.to_owned());
+                let charset = if let Some(content_type) = field.content_type() {
+                    content_type
+                        .params()
+                        .find(|x| x.0.as_str().eq_ignore_ascii_case("charset"))
+                        .map(|x| x.1.as_str().to_owned())
+                } else {
+                    None
+                };
+                let mut content = Vec::new();
+
+                while let Ok(Some(chunk)) = field.chunk().await {
+                    content.extend(chunk);
+                }
+
+                match filename {
+                    None => {
+                        let value = Self::try_decode(&content, charset);
+                        form.insert(name.to_string(), value);
+                    }
+                    Some(filename) => {
+                        file.insert(name.to_string(), (filename, content));
+                    }
+                }
+            }
+
+            self.parsed_body = ParsedRequestBody::Form(form, file);
+            return;
+        }
+
+        // fallback to Raw
+        self.parsed_body = ParsedRequestBody::Raw(body);
     }
 }
