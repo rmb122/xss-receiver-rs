81359a78d144f4d613ad61c14d1b16a0ad96ce43
rmb122 <me@rmb122.com>
2026-04-21 23:00:25 +0800

    fix: rename docker image tag

--- docker/build.sh
+++ docker/build.sh
@@ -5,5 +5,5 @@
 curl https://raw.githubusercontent.com/lionsoul2014/ip2region/refs/heads/master/data/ip2region_v4.xdb -o docker/ip2region_v4.xdb
 curl https://raw.githubusercontent.com/lionsoul2014/ip2region/refs/heads/master/data/ip2region_v6.xdb -o docker/ip2region_v6.xdb
 
-sudo docker build -f docker/Dockerfile -t ccr.ccs.tencentyun.com/rmb122/xss-receiver-backend:latest .
-sudo docker push ccr.ccs.tencentyun.com/rmb122/xss-receiver-backend:latest
+docker build -f docker/Dockerfile -t ccr.ccs.tencentyun.com/rmb122/xss-receiver-rs:latest .
+docker push ccr.ccs.tencentyun.com/rmb122/xss-receiver-rs:latest
